54bddc9e637ac39dad4b74298e383a526c479daa
Tran Thuong Thien <tranthiencdsp@gmail.com>
2016-11-25 19:51:42 +0700

    update file

---
+++ src/TechOffice/TechOffice.ViewModel/Home/ContactViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace AnThinhPhat.ViewModel.Home
+{
+    public class ContactViewModel
+    {
+        public string FullName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Title { get; set; }
+
+        public string NoiDung { get; set; }
+
+        public string CaptchaInputText { get; set; }
+    }
+}
---
+++ src/TechOffice/TechOffice.WebUI/Scripts/app/congviec-statistic.js
@@ -0,0 +1,28 @@
+﻿/// <reference path="../jquery-1.12.4.js" />
+$(document).ready(function () {
+    var optionCalendar =
+ {
+     autoSize: true,
+     constrainInput: true,
+     dateFormat: 'dd/mm/yy',
+     maxDate: new Date(),
+ };
+
+    $('.control-datepicker').datepicker(optionCalendar);
+
+    $.validator.addMethod('date',
+  function (value, element) {
+      if (this.optional(element)) {
+          return true;
+      }
+
+      var ok = true;
+      try {
+          $.datepicker.parseDate('dd/mm/yy', value);
+      }
+      catch (err) {
+          ok = false;
+      }
+      return ok;
+  });
+});
