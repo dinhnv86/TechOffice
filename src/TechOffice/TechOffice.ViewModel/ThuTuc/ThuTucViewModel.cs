c9dc47a742c3dd36db1e51dcedfa0190f4b12896
Tran Thuong Thien <tranthiencdsp@gmail.com>
2016-11-26 23:55:47 +0700

    add files

---
+++ src/TechOffice/TechOffice.ViewModel/ThuTuc/ThuTucViewModel.cs
@@ -0,0 +1,12 @@
+﻿using AnThinhPhat.Entities.Infos;
+using System.Collections.Generic;
+
+namespace AnThinhPhat.ViewModel.ThuTuc
+{
+    public class ThuTucViewModel
+    {
+        public IEnumerable<CoQuanInfo> CoQuanInfos { get; set; }
+
+        public IEnumerable<LinhVucThuTucInfo> LinhVucThuTucInfo { get; set; }
+    }
+}
