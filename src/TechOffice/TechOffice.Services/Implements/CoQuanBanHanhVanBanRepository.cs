2ba94aebd30269f55b60be28f1a9cc7d69d31d60
Tran Thuong Thien <tranthiencdsp@gmail.com>
2016-11-26 23:58:30 +0700

    add files

---
+++ src/TechOffice/TechOffice.Services/Implements/CoQuanBanHanhVanBanRepository.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using AnThinhPhat.Entities;
+using AnThinhPhat.Entities.Results;
+using AnThinhPhat.Services.Abstracts;
+using AnThinhPhat.Utilities;
+
+namespace AnThinhPhat.Services.Implements
+{
+    public class CoQuanBanHanhVanBanRepository : DbExecute, ICoQuanBanHanhVanBanRepository
+    {
+        public CoQuanBanHanhVanBanRepository(ILogService logService) : base(logService)
+        {
+        }
+
+        public SaveResult Add(CoQuanBanHanhVanBanResult entity)
+        {
+            return ExecuteDbWithHandle(_logService, () =>
+            {
+                using (var context = new TechOfficeEntities())
+                {
+                    var add = context.CoQuanBanHanhVanBans.Create();
+
+                    add.Ten = entity.Ten;
+                    add.MoTa = entity.MoTa;
+
+                    add.IsDeleted = entity.IsDeleted;
+                    add.CreatedBy = entity.CreatedBy;
+                    add.CreateDate = DateTime.Now;
+
+                    context.Entry(add).State = EntityState.Added;
+                    return context.SaveChanges() > 0 ? SaveResult.SUCCESS : SaveResult.FAILURE;
+                }
+            });
+        }
+
+        public async Task<SaveResult> AddAsync(CoQuanBanHanhVanBanResult entity)
+        {
+            return await ExecuteDbWithHandleAsync(_logService, async () =>
+            {
+                using (var context = new TechOfficeEntities())
+                {
+                    var add = context.CoQuanBanHanhVanBans.Create();
+
+                    add.Ten = entity.Ten;
+                    add.MoTa = entity.MoTa;
+
+                    add.IsDeleted = entity.IsDeleted;
+                    add.CreatedBy = entity.CreatedBy;
+                    add.CreateDate = DateTime.Now;
+
+                    context.Entry(add).State = EntityState.Added;
+                    return await context.SaveChangesAsync() > 0 ? SaveResult.SUCCESS : SaveResult.FAILURE;
+                }
+            });
+        }
+
+        public SaveResult AddRange(IEnumerable<CoQuanBanHanhVanBanResult> entities)
+        {
+            return ExecuteDbWithHandle(_logService, () =>
+            {
+                using (var context = new TechOfficeEntities())
+                {
+                    foreach (var entity in entities)
+                    {
+                        var add = context.CoQuanBanHanhVanBans.Create();
+
+                        add.Ten = entity.Ten;
+                        add.MoTa = entity.MoTa;
+
+                        add.IsDeleted = entity.IsDeleted;
+                        add.CreatedBy = entity.CreatedBy;
+                        add.CreateDate = DateTime.Now;
+
+                        context.Entry(add).State = EntityState.Added;
+                    }
+
+                    return context.SaveChanges() > 0 ? SaveResult.SUCCESS : SaveResult.FAILURE;
+                }
+            });
+        }
+
+        public async Task<SaveResult> AddRangeAsync(IEnumerable<CoQuanBanHanhVanBanResult> entities)
+        {
+            return await ExecuteDbWithHandleAsync(_logService, async () =>
+            {
+                using (var context = new TechOfficeEntities())
+                {
+                    foreach (var entity in entities)
+                    {
+                        var add = context.CoQuanBanHanhVanBans.Create();
+
+                        add.Ten = entity.Ten;
+                        add.MoTa = entity.MoTa;
+
+                        add.IsDeleted = entity.IsDeleted;
+                        add.CreatedBy = entity.CreatedBy;
+                        add.CreateDate = DateTime.Now;
+
+                        context.Entry(add).State = EntityState.Added;
+                    }
+
+                    return await context.SaveChangesAsync() > 0 ? SaveResult.SUCCESS : SaveResult.FAILURE;
+                }
+            });
+        }
+
+        public SaveResult Delete(CoQuanBanHanhVanBanResult entity)
+        {
+            return ExecuteDbWithHandle(_logService, () =>
+            {
+                using (var context = new TechOfficeEntities())
+                {
+                    var cv = context.CoQuanBanHanhVanBans.Single(x => x.Id == entity.Id && x.IsDeleted == false);
+
+                    cv.IsDeleted = true;
+                    cv.LastUpdatedBy = entity.LastUpdatedBy;
+                    cv.LastUpdated = DateTime.Now;
+
+                    context.Entry(cv).State = EntityState.Modified;
+                    return context.SaveChanges() > 0 ? SaveResult.SUCCESS : SaveResult.FAILURE;
+                }
+            });
+        }
+
+        public async Task<SaveResult> DeleteAsync(CoQuanBanHanhVanBanResult entity)
+        {
+            return await ExecuteDbWithHandleAsync(_logService, async () =>
+            {
+                using (var context = new TechOfficeEntities())
+                {
+                    var cv = context.CoQuanBanHanhVanBans.Single(x => x.Id == entity.Id && x.IsDeleted == false);
+
+                    cv.IsDeleted = true;
+                    cv.LastUpdatedBy = entity.LastUpdatedBy;
+                    cv.LastUpdated = DateTime.Now;
+
+                    context.Entry(cv).State = EntityState.Modified;
+
+                    return await context.SaveChangesAsync() > 0 ? SaveResult.SUCCESS : SaveResult.FAILURE;
+                }
+            });
+        }
+
+        public SaveResult DeleteBy(int id)
+        {
+            return ExecuteDbWithHandle(_logService, () =>
+            {
+                using (var context = new TechOfficeEntities())
+                {
+                    var cv = context.CoQuanBanHanhVanBans.Single(x => x.Id == id && x.IsDeleted == false);
+
+                    cv.IsDeleted = true;
+                    cv.LastUpdated = DateTime.Now;
+
+                    context.Entry(cv).State = EntityState.Modified;
+
+                    return context.SaveChanges() > 0 ? SaveResult.SUCCESS : SaveResult.FAILURE;
+                }
+            });
+        }
+
+        public async Task<SaveResult> DeleteByAsync(int id)
+        {
+            return await ExecuteDbWithHandleAsync(_logService, async () =>
+            {
+                using (var context = new TechOfficeEntities())
+                {
+                    var cv = context.CoQuanBanHanhVanBans.Single(x => x.Id == id && x.IsDeleted == false);
+                    cv.IsDeleted = true;
+
+                    context.Entry(cv).State = EntityState.Modified;
+
+                    return await context.SaveChangesAsync() > 0 ? SaveResult.SUCCESS : SaveResult.FAILURE;
+                }
+            });
+        }
+
+        public IEnumerable<CoQuanBanHanhVanBanResult> GetAll()
+        {
+            return ExecuteDbWithHandle(_logService, () =>
+            {
+                using (var context = new TechOfficeEntities())
+                {
+                    return (from item in context.CoQuanBanHanhVanBans
+                            where item.IsDeleted == false
+                            select item)
+                        .MakeQueryToDatabase()
+                        .Select(x => x.ToDataResult())
+                        .ToList();
+                }
+            });
+        }
+
+        public async Task<IEnumerable<CoQuanBanHanhVanBanResult>> GetAllAsync()
+        {
+            return await ExecuteDbWithHandleAsync(_logService, async () =>
+            {
+                using (var context = new TechOfficeEntities())
+                {
+                    return await (from item in context.CoQuanBanHanhVanBans
+                                  where item.IsDeleted == false
+                                  select item)
+                                  .MakeQueryToDatabase()
+                                  .Select(x => x.ToDataResult())
+                                  .AsQueryable()
+                                  .ToListAsync();
+                }
+            });
+        }
+
+        public IEnumerable<CoQuanBanHanhVanBanResult> GetAllWithChildren()
+        {
+            return ExecuteDbWithHandle(_logService, () =>
+            {
+                using (var context = new TechOfficeEntities())
+                {
+                    return (from item in context.CoQuanBanHanhVanBans
+                            where item.IsDeleted == false
+                            select item)
+                        .MakeQueryToDatabase()
+                        .Select(x => x.ToDataResult())
+                        .ToList();
+                }
+            });
+        }
+
+        public CoQuanBanHanhVanBanResult Single(int id)
+        {
+            return ExecuteDbWithHandle(_logService, () =>
+            {
+                using (var context = new TechOfficeEntities())
+                {
+                    return (from item in context.CoQuanBanHanhVanBans
+                            where item.IsDeleted == false &&
+                                  item.Id == id
+                            select item)
+                            .MakeQueryToDatabase()
+                            .Select(x => x.ToDataResult())
+                            .Single();
+                }
+            });
+        }
+
+        public async Task<CoQuanBanHanhVanBanResult> SingleAsync(int id)
+        {
+            return await ExecuteDbWithHandleAsync(_logService, async () =>
+            {
+                using (var context = new TechOfficeEntities())
+                {
+                    return await (from item in context.CoQuanBanHanhVanBans
+                                  where item.IsDeleted == false &&
+                                        item.Id == id
+                                  select item)
+                                  .MakeQueryToDatabase()
+                                  .Select(x => x.ToDataResult())
+                                  .AsQueryable()
+                                  .SingleAsync();
+                }
+            });
+        }
+
+        public SaveResult Update(CoQuanBanHanhVanBanResult entity)
+        {
+            return ExecuteDbWithHandle(_logService, () =>
+            {
+                using (var context = new TechOfficeEntities())
+                {
+                    var update = context.CoQuanBanHanhVanBans.Single(x => x.Id == entity.Id && x.IsDeleted == false);
+
+                    update.Ten = entity.Ten;
+                    update.MoTa = entity.MoTa;
+                    update.IsDeleted = entity.IsDeleted;
+                    update.LastUpdatedBy = entity.LastUpdatedBy;
+                    update.LastUpdated = DateTime.Now;
+
+                    context.Entry(update).State = EntityState.Modified;
+
+                    return context.SaveChanges() > 0 ? SaveResult.SUCCESS : SaveResult.FAILURE;
+                }
+            });
+        }
+
+        public async Task<SaveResult> UpdateAsync(CoQuanBanHanhVanBanResult entity)
+        {
+            return await ExecuteDbWithHandleAsync(_logService, async () =>
+            {
+                using (var context = new TechOfficeEntities())
+                {
+                    var update = context.CoQuanBanHanhVanBans.Single(x => x.Id == entity.Id && x.IsDeleted == false);
+
+                    update.Ten = entity.Ten;
+                    update.MoTa = entity.MoTa;
+                    update.IsDeleted = entity.IsDeleted;
+                    update.LastUpdatedBy = entity.LastUpdatedBy;
+                    update.LastUpdated = DateTime.Now;
+
+                    context.Entry(update).State = EntityState.Modified;
+
+                    return await context.SaveChangesAsync() > 0 ? SaveResult.SUCCESS : SaveResult.FAILURE;
+                }
+            });
+        }
+    }
+}
