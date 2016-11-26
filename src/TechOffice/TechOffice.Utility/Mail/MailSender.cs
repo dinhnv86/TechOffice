450eb892d539367bd55e68ac97c4cfe80c98b7b0
Tran Thuong Thien <tranthiencdsp@gmail.com>
2016-11-26 09:31:11 +0700

    add file MailSender

---
+++ src/TechOffice/TechOffice.Utility/Mail/MailSender.cs
@@ -0,0 +1,50 @@
+﻿using System.Net.Mail;
+
+namespace AnThinhPhat.Utilities.Mail
+{
+    public static class MailSender
+    {
+        public static void SendRegister(string email, string subject, string linkConfirm)
+        {
+            int port = TechOfficeConfig.PORT;
+            string host = TechOfficeConfig.HOST;
+            string from = TechOfficeConfig.FROM;
+            string user = TechOfficeConfig.USERNAME;
+            string pass = TechOfficeConfig.PASSWORD;
+
+            SmtpMailSender.Email(host, email, "", subject, from, "Phong Noi Vu Huyen Phu Rieng", user, pass, null, linkConfirm, null, MailType.Register, MailPriority.High);
+        }
+
+        public static void SendFeedback(string email, string subject, MailAttachment[] files)
+        {
+            int port = TechOfficeConfig.PORT;
+            string host = TechOfficeConfig.HOST;
+            string from = TechOfficeConfig.FROM;
+            string user = TechOfficeConfig.USERNAME;
+            string pass = TechOfficeConfig.PASSWORD;
+
+            SmtpMailSender.Email(host, email, "", subject, from, "Phong Noi Vu Huyen Phu Rieng", user, pass, files, "", null, MailType.Feedback, MailPriority.High);
+        }
+
+        public static void SendRecoverPassword(string email, string subject, string linkConfirm, string plainText)
+        {
+            int port = TechOfficeConfig.PORT;
+            string host = TechOfficeConfig.HOST;
+            string from = TechOfficeConfig.FROM;
+            string user = TechOfficeConfig.USERNAME;
+            string pass = TechOfficeConfig.PASSWORD;
+
+            SmtpMailSender.Email(host, email, "", subject, from, "Phong Noi Vu Huyen Phu Rieng", user, pass, null, linkConfirm, plainText, MailType.RecoverPass, MailPriority.High);
+        }
+
+        public static void Send(string to, string subject, string body)
+        {
+            int port = TechOfficeConfig.PORT;
+            string host = TechOfficeConfig.HOST;
+            string from = TechOfficeConfig.FROM;
+            string user = TechOfficeConfig.USERNAME;
+            string pass = TechOfficeConfig.PASSWORD;
+            SmtpMailSender.Email(host, to, body, subject, from, "Phong Noi Vu Huyen Phu Rieng", user, pass, null, null, null, MailType.Normal, MailPriority.Normal);
+        }
+    }
+}
