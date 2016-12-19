9ac41736f197217242d70236a572bc84a6b3e208
Tran Thuong Thien <tranthiencdsp@gmail.com>
2016-12-19 19:45:00 +0700

    add thers's some missing files

---
+++ src/TechOffice/TechOffice.Entities/Results/NewsResult.cs
@@ -0,0 +1,19 @@
+﻿namespace AnThinhPhat.Entities.Results
+{
+    public class NewsResult : LogResult
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public string Summary { get; set; }
+
+        public int NewsCategoryId { get; set; }
+
+        public string UrlImage { get; set; }
+
+        public NewsCategoryResult NewsCategory { get; set; }
+    }
+}
